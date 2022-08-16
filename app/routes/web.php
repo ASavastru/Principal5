1cde7ff742546752bf62bc931486d8b59ac1ea32
ASavastru <smeag0leye>
2022-08-16 13:04:30 +0300

    Mihail Base

---
+++ app/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/** @var Router $router */
+global $router;
+
+/** @var Container $container */
+global $container;
+
+use App\Controllers\{Auth\LoginController, Auth\LogoutController, Auth\RegisterController, HomeController};
+use App\Middleware\{Authenticated, Guest};
+use League\{Container\Container, Route\RouteGroup, Route\Router};
+
+
+// Routes that need authentication in order to access
+$router->group('', function (RouteGroup $router) {
+
+    $router->get('/', [HomeController::class, 'index'])->setName('home');
+
+    $router->post('/logout', [LogoutController::class, 'logout'])->setName('logout');
+
+})->middleware($container->get(Authenticated::class));
+
+// Routes that can be accessed only if the user is NOT authenticated
+$router->group('', function (RouteGroup $router) {
+
+    $router->get('/login', [LoginController::class, 'index'])->setName('login');
+
+    $router->post('/login', [LoginController::class, 'store'])->setName('login.store');
+
+    $router->get('/register', [RegisterController::class, 'index'])->setName('register');
+    //has a form for get
+
+    $router->post('/register', [RegisterController::class, 'store'])->setName('register.store');
+    //has a form for post
+
+})->middleware($container->get(Guest::class));
